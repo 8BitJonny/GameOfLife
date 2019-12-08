25d9652ebd4fb117f531828023630aae8f6c3ea4
8BitJonny <timon99@gmx.de>
2019-12-08 21:44:26 +0100

    Updates

---
+++ precache-manifest.97d329bcda54ef8a3e7ed082b9f55a8d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b21b38872cdc5f0f376dfd652124944d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b59ee563457e03c488a8",
+    "url": "/static/css/main.51f1e738.chunk.css"
+  },
+  {
+    "revision": "045653eab19e1d9a2ad3",
+    "url": "/static/js/2.824c1d53.chunk.js"
+  },
+  {
+    "revision": "b59ee563457e03c488a8",
+    "url": "/static/js/main.616a2d59.chunk.js"
+  },
+  {
+    "revision": "106e3534030530955da4",
+    "url": "/static/js/runtime-main.0a5eb029.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/GameOfLife/precache-manifest.d8904808f8c94af193066c22d68145dd.js"
+  "/precache-manifest.97d329bcda54ef8a3e7ed082b9f55a8d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/GameOfLife/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
