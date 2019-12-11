8fbb4bdf5508aae11894979f76240b906717374d
8BitJonny <timon99@gmx.de>
2019-12-11 15:46:23 +0100

    :earth_africa: Nuxt Big Bang

--- src/model/grid.ts
+++
@@ -1,7 +0,0 @@
-import Alive from "./alive";
-
-interface Grid extends Array<Array<Alive>> {
-	[index: number]: Alive[];
-}
-
-export default Grid;
--- src/utils.ts
+++
@@ -1,3 +0,0 @@
-export default function remap(oldValue: number, oldRange:{min: number, max: number}, newRange:{min: number, max: number}) {
-	return (((oldValue - oldRange.min) * (newRange.max - newRange.min)) / (oldRange.max - oldRange.min)) + newRange.min;
-}
